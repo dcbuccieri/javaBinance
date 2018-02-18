da97b1f1217d99469efb576ea65368d638956add
David Buccieri <dcbuccieri@hotmail.com>
2018-02-18 16:37:33 -0500

    first commit. compiles and runs.

---
+++ src/main/java/myPackage/myClass.java
@@ -0,0 +1,18 @@
+package myPackage;
+
+import com.webcerebrium.binance.api.BinanceApi;
+import com.webcerebrium.binance.api.BinanceApiException;
+
+
+public class myClass {
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		//System.out.println("Hello World!!!");
+		try {
+			  BinanceApi api = new BinanceApi();
+			  System.out.println("ETH-BTC PRICE=" + api.pricesMap().get("ETHBTC"));
+			} catch (BinanceApiException e) {
+			  System.out.println( "ERROR: " + e.getMessage());
+			}
+	}
+}
